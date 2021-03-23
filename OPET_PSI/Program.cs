d15053ad816d40602e52f5148a4f5eb1319fa2ef
student <Student@B420-01.fesb.hr>
2021-03-23 20:18:57 +0100

    Add project files.

---
+++ OPET_PSI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OPET_PSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Whorld!");
+        }
+    }
+}
